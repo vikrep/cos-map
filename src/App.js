de8c33b3c1d5fa694f3b6d01136dba52d06861ca
vikrep <protsenko.viktor@gmail.com>
2018-09-18 10:16:54 +0100

    added MapContainer

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import { Route, BrowserRouter as Router, } from 'react-router-dom'
 import './App.css';
 import InputForm from './components/InputForm.js';
-import Map from './components/Map.js';
+import MapContainer from './components/MapContainer.js';
 
 class App extends Component {
     render() {
         return (
             <Router>
                 <div className="App">
-                    <Route exact path="/" component={Map} />
+                    <Route exact path="/" component={MapContainer} />
                     <Route path="/form" component={InputForm} />
                 </div>
             </Router>
---
+++ src/components/MapContainer.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
+import { compose, withProps } from "recompose";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import { API_GOOGLE } from './config/googleApi.js'
+
+const MyMapComponent = compose(
+    withProps({
+        googleMapURL: API_GOOGLE,
+        loadingElement: <div style={{ height: `90%` }} />,
+        containerElement: <div style={{ height: `400px` }} />,
+        mapElement: <div style={{ height: `100%` }} />
+    }),
+    withScriptjs,
+    withGoogleMap
+)(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: 53.5500, lng: -2.4333 }}>
+        {props.isMarkerShown && (
+            <Marker position={{ lat: 53.485905, lng: -2.242330 }} />
+        )}
+    </GoogleMap>
+));
+
+
+class MapContainer extends Component {
+    state = {
+        isMarkerShown: false,
+    }
+
+    componentDidMount() {
+        this.delayedShowMarker()
+    }
+
+    delayedShowMarker = () => {
+        setTimeout(() => {
+            this.setState({ isMarkerShown: true })
+        }, 3000)
+    }
+
+    handleMarkerClick = () => {
+        this.setState({ isMarkerShown: false })
+        this.delayedShowMarker()
+    }
+    render() {
+        return (
+            <div className="map">
+                <h2>Here should be Map</h2>
+                <MyMapComponent isMarkerShown={this.state.isMarkerShown}
+                    onMarkerClick={this.handleMarkerClick} />
+                <Link to="/form"><Button floated="center" color="blue">Go to Form Data</Button></Link>
+            </div>
+        )
+    }
+}
+
+export default MapContainer
