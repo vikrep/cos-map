c3df0a327c1c76283d409cae5a33b80b5c631231
vikrep <protsenko.viktor@gmail.com>
2018-09-19 23:33:09 +0100

    pop-up window added

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'semantic-ui-react';
-import { compose, withProps } from "recompose";
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import { compose, withProps, withStateHandlers } from "recompose";
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps";
 //you should add your API_key and put this line to /source/components/config/googleApi.js
-import { API_GOOGLE } from './config/googleApi.js' 
+import { API_GOOGLE } from './config/googleApi.js'
 
 
+
+const markericon = { url: 'https://cityofsanctuary.org/wp-content/uploads/2018/09/8506DB46-675C-48E8-9081-C4672BF226D6.png', size: {width: 45, height: 46}, anchor: {x: 17, y: 34}, scaledSize: {width: 15, height: 15} }
 const MyMapComponent = compose(
+    withStateHandlers(() => ({
+        isOpen: false, }),
+         { onToggleOpen: ({ isOpen }) => () => ({ isOpen: !isOpen, })
+      }),
     withProps({
         googleMapURL: API_GOOGLE,
         loadingElement: <div style={{ height: `100%` }} />,
@@ -19,8 +25,11 @@ const MyMapComponent = compose(
 )(props => (
     <GoogleMap defaultZoom={8} defaultCenter={{ lat: 53.5500, lng: -2.4333 }}>
         {props.isMarkerShown && (
-            <Marker position={{ lat: 53.485905, lng: -2.242330 }} />
+            <Marker icon={markericon} position={{ lat: 53.485905, lng: -2.242330 }} onClick={props.onToggleOpen} />
         )}
+        {props.isOpen && <InfoWindow  position={{ lat: 53.485905, lng: -2.242330 }} onCloseClick={props.onToggleOpen}>
+        <div className="marker-text">Hello world! This is test mode pop-up window</div>
+      </InfoWindow>}
     </GoogleMap>
 ));
 
@@ -37,7 +46,7 @@ class MapContainer extends Component {
     delayedShowMarker = () => {
         setTimeout(() => {
             this.setState({ isMarkerShown: true })
-        }, 3000)
+        }, 1000)
     }
 
     handleMarkerClick = () => {
