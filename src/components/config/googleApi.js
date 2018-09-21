96b74d2ec5e196de83f9de1c8427b95dc87eb644
vikrep <protsenko.viktor@gmail.com>
2018-09-21 11:09:02 +0100

    changed api

--- src/components/config/googleApi.js
+++ src/components/config/googleApi.js
@@ -1,1 +1,1 @@
-export const API_GOOGLE = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDrqgsWQHSd3tVM9WNxhZTFzD3s1sAlOgs&language=en&region=UK&v=3.exp&libraries=geometry,drawing,places"
+export const API_GOOGLE = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCUhvBWpGvds4X1doWLvT8qxqadi8wzz3g&language=en&region=UK&v=3.exp&libraries=geometry,drawing,places"
