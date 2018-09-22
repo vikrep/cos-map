7d93a1227f7996b1c5166db07fe91b1651710219
Viktor Protsenko <30617331+vikrep@users.noreply.github.com>
2018-09-22 12:21:43 +0100

    delete credentials

--- src/components/config/firebase.js
+++
@@ -1,8 +0,0 @@
-export var config = {
-    apiKey: "AIzaSyANV0TcRPdHAKAY8ipGhLZmS1qfhzS6gP0",
-    authDomain: "cos-map-6b449.firebaseapp.com",
-    databaseURL: "https://cos-map-6b449.firebaseio.com",
-    projectId: "cos-map-6b449",
-    storageBucket: "cos-map-6b449.appspot.com",
-    messagingSenderId: "252411541405"
-};
